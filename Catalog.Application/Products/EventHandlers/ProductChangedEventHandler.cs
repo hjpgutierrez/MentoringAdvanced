bd7330200629f283a66e05f20a74982dbb4c7cdf
hjpgutierrez <hpaezgutierrez@gmail.com>
2025-05-17 16:12:59 -0500

    - Publish message-events into RabbitMQ when products are modified

---
+++ Catalog.Application/Products/EventHandlers/ProductChangedEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Application.Common.Interfaces;
+using Catalog.Domain.Events;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Application.Products.EventHandlers;
+public class ProductChangedEventHandler : INotificationHandler<ProductChangedEvent>
+{
+    private readonly ILogger<ProductChangedEventHandler> _logger;
+    private readonly IMessagePublisher _messagePublisher;
+
+    public ProductChangedEventHandler(ILogger<ProductChangedEventHandler> logger, IMessagePublisher messagePublisher)
+    {
+        _logger = logger;
+        _messagePublisher = messagePublisher;
+    }
+
+    public async Task Handle(ProductChangedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Domain Event: {DomainEvent}", notification.GetType().Name);
+
+        await _messagePublisher.PublishAsync(notification, "product");
+
+        await Task.CompletedTask;
+    }
+}
+
---
+++ Catalog.Infrastructure/MessageBrokers/RabbitMqMessagePublisher.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Application.Common.Interfaces;
+using System.Text;
+using RabbitMQ.Client;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Infrastructure.MessageBrokers
+{
+    public class RabbitMqMessagePublisher : IMessagePublisher
+    {
+        private readonly ILogger<RabbitMqMessagePublisher> _logger;
+
+        public RabbitMqMessagePublisher(ILogger<RabbitMqMessagePublisher> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task PublishAsync<T>(T message, string routingKey)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+            await channel.QueueDeclareAsync(queue: routingKey, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var json = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: routingKey, body: body);
+            _logger.LogInformation($"Sent {message}");
+            await Task.CompletedTask;
+        }
+    }
+}
