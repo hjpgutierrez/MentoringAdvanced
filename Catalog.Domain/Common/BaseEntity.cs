aeeb831973713386e485cdd241f4dada02cff66f
hjpgutierrez <hpaezgutierrez@gmail.com>
2025-06-20 22:33:59 -0500

    Add custom rules

--- Catalog.Domain/Common/BaseEntity.cs
+++ Catalog.Domain/Common/BaseEntity.cs
@@ -13,19 +13,10 @@ public abstract class BaseEntity
     [NotMapped]
     public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
 
-    public void AddDomainEvent(BaseEvent domainEvent)
-    {
-        _domainEvents.Add(domainEvent);
-    }
+    public void AddDomainEvent(BaseEvent domainEvent) => _domainEvents.Add(domainEvent);
 
-    public void RemoveDomainEvent(BaseEvent domainEvent)
-    {
-        _domainEvents.Remove(domainEvent);
-    }
+    public void RemoveDomainEvent(BaseEvent domainEvent) => _domainEvents.Remove(domainEvent);
 
-    public void ClearDomainEvents()
-    {
-        _domainEvents.Clear();
-    }
+    public void ClearDomainEvents() => _domainEvents.Clear();
 
 }
